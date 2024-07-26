268ad7e5fb6b43599391bce7b099e375104cc393
devotus8915 <nadia050809@gmail.com>
2024-07-26 17:18:11 +0900

    2번째
    Tilemap 수정
    EnemySpawner 생성
    TowerSpawner 제작
    - Tilemap 위로 생성 안 됨
    - Tilemap 부분 수정 필요할 것 같음

--- Tower Defence/Assets/Scripts/Enemy.cs
+++ Tower Defence/Assets/Scripts/Enemy.cs
@@ -37,11 +37,34 @@ public class Enemy : MonoBehaviour
             /*적의 현재 위치와 목표위치의 거리가 0.02 * movement2D.MoveSpeed보다 적을 때 if 조건문 실행
              movement2D.MoveSpeed를 곱해주는 이유는 속도가 빠르면 한 프레임에 0.02보다 크게 움직이기 때문에
             if 조건문에 걸리지 않고 경로를 탈주하는 오브젝트가 발생할 수 있다.*/
+
+            if(Vector3.Distance(transform.position, wayPoints[currentIndex].position) < 0.02f * movement2D.MoveSpeed)
+            {
+                //다음 이동 방향 설정
+                NextMoveTo();
+            }
+
+            yield return null;
         }
     }
 
     private void NextMoveTo()
     {
-
+        //아직 이동할 wayPoints가 남아있다면
+        if(currentIndex < wayPointCount - 1)
+        {
+            //적의 위치를 정확하게 목표 위치로 설정
+            transform.position = wayPoints[currentIndex].position;
+            //이동 방향 설정 => 다음 목표지점(wayPoints)
+            currentIndex++;
+            Vector3 direction = (wayPoints[currentIndex].position-transform.position).normalized;
+            movement2D.MoveTo(direction);
+        }
+        //현재 위치가 마지막 wayPoints이면
+        else
+        {
+            //적 오브젝트 삭제
+            Destroy(gameObject);
+        }
     }
 }
